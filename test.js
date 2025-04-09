21f82b23746503f453166edd4e25defae290ada4
NiaNia2 <lacodeuse@protonmail.com>
2025-04-09 11:58:56 +0200

    ajout exos_1

--- test.js
+++ test.js
@@ -1,7 +1,13 @@
 const numbers = [1, 2, 3, 4]
+// for (let i = 0; i < numbers.map; i++) {
 const newNumbers = numbers.map((numbers) => {
     return numbers * 2
 })
 
 
 console.log(newNumbers)
+
+
+
+
+
