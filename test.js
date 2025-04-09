dff3689a36bf30928531f773c74c1d6b8cc50049
NiaNia2 <lacodeuse@protonmail.com>
2025-04-09 12:14:54 +0200

    ajout exos_8

--- test.js
+++ test.js
@@ -1,11 +1,4 @@
-const numbers = [1, 2, 3, 4]
-// for (let i = 0; i < numbers.map; i++) {
-const newNumbers = numbers.map((numbers) => {
-    return numbers * 2
-})
 
-
-console.log(newNumbers)
 
 
 
